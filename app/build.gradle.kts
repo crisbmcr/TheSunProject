eb9aaa8cc309ae16f1e50baf13d8968137ad967e
Cristian Bedia <crisb.mcr7@gmail.com>
2026-03-26 01:54:19 -0300

    pruebas de refinamiento de atlas debug y atlas projected10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,7 +78,7 @@ configurations.all {
 
 dependencies {
     // --- DEPENDENCIA AL MÓDULO DE OPENCV ---
-    implementation("org.opencv:opencv:4.13.0")
+    implementation("org.opencv:opencv:4.10.0")
     // --- DEPENDENCIAS EXISTENTES ---
     implementation(libs.androidx.core.ktx)
     implementation("androidx.appcompat:appcompat:1.6.1")
